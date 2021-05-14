fcc8b55caa5363627f9c361c9e1af8389ea344dd
Bankowski Jakub 2001 <bania1414@gmail.com>
2021-05-14 19:40:25 +0200

    przygotowanie RP_2D_wireworld_animation pod wyswietlanie wrld

--- src/GUI/RP_2D_wireworld_animation.java
+++ src/GUI/RP_2D_wireworld_animation.java
@@ -8,19 +8,20 @@ import java.awt.event.ActionListener;
 public class RP_2D_wireworld_animation extends JPanel implements ActionListener {
 
 
-    int rows = 100;
-    int columns = 100;
-    int rec_WIDTH = 10;
-    int rec_HEIGHT = 10;
-    int x = 1;
-    int y = 1;
+    private final int rows = 100;
+    private final int columns = 100;
+    private final int cell_WIDTH =10;
+    private final int cell_HEIGHT =10;
+    private final int rec_WIDTH = 10;
+    private final int rec_HEIGHT = 10;
+    int [][] wrld= new int[rows][columns];
 
     Timer timer;
 
     public RP_2D_wireworld_animation (int f){
-        this.setPreferredSize(new Dimension(columns*rec_WIDTH,rows*rec_HEIGHT));
+        this.setPreferredSize(new Dimension(columns*cell_WIDTH,rows*cell_HEIGHT));
         this.setBackground(Color.black);
-        timer = new Timer(10,this);
+        timer = new Timer(1000,this);
         timer.start();
 
     }
@@ -30,22 +31,23 @@ public class RP_2D_wireworld_animation extends JPanel implements ActionListener 
         Graphics2D g2D = (Graphics2D)g;
 
         for(int i = 0; i<rows;i++){
-            for(int j = 0; j<rows;j++){
-                g2D.setPaint(Color.white);
-                g2D.fillRect(i*rec_HEIGHT,j*rec_WIDTH,8,8);
+            for(int j = 0; j<columns;j++){
+                wrld[i][j]= (int) (Math.random() * ((4) + 1));
+                if(wrld[i][j]==0){
+                    g2D.setPaint(Color.black);
+                }else if (wrld[i][j]==1){
+                    g2D.setPaint(Color.white);
+                }else if(wrld[i][j]==2){
+                    g2D.setPaint(Color.yellow);
+                }else g2D.setPaint(Color.blue);
+
+                g2D.fillRect(i*cell_HEIGHT,j*cell_WIDTH,rec_WIDTH,rec_HEIGHT);
             }
         }
-
-        g2D.setPaint(Color.blue);
-        g2D.drawRect(x,y,100,100);
-
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        x=x+1;
-        y=y+1;
-
 
         repaint();
     }
