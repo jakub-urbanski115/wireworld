c0e756b80c03d34f073fcf1afd74d640b79b6bfd
Bankowski Jakub 2001 <bania1414@gmail.com>
2021-05-14 15:30:53 +0200

    Main

---
+++ src/DMain/Main.java
@@ -0,0 +1,12 @@
+package DMain;
+
+import GUI.First_Window;
+
+public class Main {
+    public void main(String[] args) {
+        init_gui();
+    }
+    public void init_gui (){
+        First_Window f = new First_Window();
+    }
+}
--- src/GUI/GUI.java
+++
@@ -1,9 +0,0 @@
-package GUI;
-
-public class GUI {
-
-    public static final void main(String[] args) {
-        First_Window f = new First_Window();
-
-    }
-}
