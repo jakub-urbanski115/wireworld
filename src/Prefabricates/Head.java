4770842c39e69f169279df6765882e60da37602e
jakub_urbanski <jakub.urbanski115@wp.pl>
2021-05-16 16:13:32 +0200

    poprawa oznaczen komórek

--- src/Prefabricates/Head.java
+++ src/Prefabricates/Head.java
@@ -7,7 +7,7 @@ public class Head {
     public void draw_head(int x, int y){
         World world = new World();
         if(collision_head(x,y) == 0)
-            world.overwrite(x,y,2);
+            world.overwrite(x,y,1);
     }
 
     private int collision_head(int x, int y){
@@ -15,8 +15,8 @@ public class Head {
         int [][] copied = world.copy_world();
         // dla elektronu nalezy sprawdzic czy rysujemy go "na kablu", lub czy nie ma juz w tym miejscu innego elektronu
         if(copied[x][y] == 0) return 1;
+        else if(copied[x][y] == 1) return 1;
         else if(copied[x][y] == 2) return 1;
-        else if(copied[x][y] == 3) return 1;
         else{return 0;}
     }
 }
--- src/Prefabricates/Tail.java
+++ src/Prefabricates/Tail.java
@@ -7,7 +7,7 @@ public class Tail {
     public void draw_tail(int x, int y){
         World world = new World();
         if(collision_tail(x,y) == 0)
-            world.overwrite(x,y,3);
+            world.overwrite(x,y,2);
     }
 
     private int collision_tail(int x, int y){
@@ -15,8 +15,8 @@ public class Tail {
         int [][] copied = world.copy_world();
         // dla elektronu nalezy sprawdzic czy rysujemy go "na kablu", lub czy nie ma juz w tym miejscu innego elektronu
         if(copied[x][y] == 0) return 1;
+        else if(copied[x][y] == 1) return 1;
         else if(copied[x][y] == 2) return 1;
-        else if(copied[x][y] == 3) return 1;
         else{return 0;}
     }
 }
